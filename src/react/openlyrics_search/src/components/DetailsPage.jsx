1e59a5b8be0d37a8669eb68ef9f585c0c41efba5
Erick13as <alvaradosolanoerick@gmail.com>
2023-05-27 18:49:00 -0600

    Update DetailsPage.jsx

--- src/react/openlyrics_search/src/components/DetailsPage.jsx
+++ src/react/openlyrics_search/src/components/DetailsPage.jsx
@@ -4,14 +4,15 @@ import { auth } from "../firebase/firebaseConfig";
 import { useNavigate } from "react-router-dom";
 
 const DetailsPage = () => {
-  const [data, setData] = useState([]);
+  const [songName, setSongName] = useState("");
   const navigate = useNavigate();
 
   const fetchData = () => {
     fetch("https://main-app.mangoocean-f33b36da.eastus.azurecontainerapps.io/open-lyrics-search/songs/6471c35b55b8c9931ee5b155")
       .then((response) => response.json())
       .then((jsonData) => {
-        setData(jsonData[0]);
+        setSongName(jsonData.song_name);
+        console.log(jsonData);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -32,12 +33,9 @@ const DetailsPage = () => {
 
   return (
     <div className="search_page-container">
-      <div className="dataContainer">
-        {/* Render the fetched data */}
-        <div key={data._id}>{data.song_name}</div>
-      </div>
 
       <form className="formContainer">
+        <h1 className="title">{songName}</h1>
         <button onClick={() => navigate("/mainPage")} type="button" className="buttons">
           Volver
         </button>
