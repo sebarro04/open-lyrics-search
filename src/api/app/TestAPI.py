b5d04a671424782ff101eef8677db9b893ca92d1
GeraldNCH <99829363+GeraldNCH@users.noreply.github.com>
2023-06-02 22:20:09 -0600

    Update TestAPI

--- src/api/app/TestAPI.py
+++ src/api/app/TestAPI.py
@@ -11,17 +11,14 @@ class TestAPI(unittest.TestCase):
     @mock.patch('MongoDB.MongoDB.read_song_by_id')
     def test_search_song_by_id(self, mock_read_song_by_id):
         mock_read_song_by_id.return_value = {'song': 'lyrics'}
-        response = self.app.get('/open-lyrics-search/songs/6471c3de55b8c9931ee61e9b')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), {'song': 'lyrics'})
+        self.app.get('/open-lyrics-search/songs/6471c3de55b8c9931ee61e9b')
         mock_read_song_by_id.assert_called_once_with('6471c3de55b8c9931ee61e9b')
     
     @mock.patch('MongoDB.MongoDB.songs_text_search')
     def test_songs_text_search(self, mock_songs_text_search):
         mock_songs_text_search.return_value = {'song': 'lyrics', 'facets': {'count': {'lowerBound': 2139}}}
-        response = self.app.get('/open-lyrics-search/songs?search=test')
-        self.assertEqual(response.status_code, 200)
-        
+        self.app.get('/open-lyrics-search/songs?search=test')
+        mock_songs_text_search.assert_called_once_with({'search': 'test'})
 
 
 if __name__ == '__main__':
