cd5d47bdc6e3335ff72ca0c520bcb25e7f7aefba
GeraldNCH <99829363+GeraldNCH@users.noreply.github.com>
2023-06-02 22:07:29 -0600

    Update TestAPI

--- src/api/app/TestAPI.py
+++ src/api/app/TestAPI.py
@@ -16,7 +16,13 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.get_json(), {'song': 'lyrics'})
         mock_read_song_by_id.assert_called_once_with('6471c3de55b8c9931ee61e9b')
     
-    
+    @mock.patch('MongoDB.MongoDB.songs_text_search')
+    def test_songs_text_search(self, mock_songs_text_search):
+        mock_songs_text_search.return_value = {'song': 'lyrics', 'facets': {'count': {'lowerBound': 2139}}}
+        response = self.app.get('/open-lyrics-search/songs?search=test')
+        self.assertEqual(response.status_code, 200)
+        
+
 
 if __name__ == '__main__':
     unittest.main()
